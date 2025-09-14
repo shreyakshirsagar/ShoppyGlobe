2129ff7b0343ced3cbe05581d9b2d5e377f126a4
shreyakshirsagar <shreyakshirsagar8767@gmail.com>
2025-09-14 11:25:30 +0530

    feat: add mobile menu state management to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './Header.css';
 
 const Header = () => {
   const totalQuantity = useSelector(state => state.cart.totalQuantity);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <header className="header">
